a0a64ea59fba32b81ed69e4359c2316b480b3062
selinabayly <selinabayly@gmail.com>
2021-03-13 21:32:15 +0000

    First push and commit to Git

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print('Hello')
+print('Hello again')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

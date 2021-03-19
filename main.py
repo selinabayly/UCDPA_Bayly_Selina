4bf4429945deda4eaf0c5ad62d5860e85105c896
selinabayly <selinabayly@gmail.com>
2021-03-19 01:05:00 +0000

    Correct Data

--- main.py
+++ main.py
@@ -26,37 +26,83 @@ Reading_Order_All = BeautifulSoup(Reading_Order_Source,'lxml')
 # Publication Order of Discworld Books
 Reading_Order = Reading_Order_All.find('table')
 
-# Loop through Table to extract title and year+
+# Loop through Table to extract title and year
 
 build_table = []
+counter = 0
 for tr in Reading_Order.find_all('tr'):
+    counter = counter + 1
+    book_id = counter
     title_elem = tr.find('td', class_='booktitle').text
     year_elem = tr.find('td',class_ = 'bookyear').text
-    build_table.append((title_elem,year_elem))
+    build_table.append((book_id,title_elem,year_elem))
 
 # Create table and then remove brackets from year
-cols = ['Book_Title','Book_Year']
+cols = ['Book_Id','Book_Title','Book_Year']
 DW_Publication_Order = pd.DataFrame(build_table, columns=cols)
 
 DW_Publication_Order['Book_Year'] = DW_Publication_Order['Book_Year'].str.replace('(','')
 DW_Publication_Order['Book_Year'] = DW_Publication_Order['Book_Year'].str.replace(')','')
 
-#print(DW_Publication_Order)
+print(DW_Publication_Order.head())
 
 ##################################################
 # Import Character Order from XLSX
 ##################################################
 
 DW_Character_Order = pd.read_excel(r'DW_Public_Genre_Order.xlsx')
-print(DW_Character_Order)
+
+##################################################
+# Do data checks between two DW dataframes
+# Year, Title
+##################################################
+
+# Check number of columns in the two tables = 41
+print(DW_Publication_Order.shape)
+print(DW_Publication_Order.columns)
+print(DW_Character_Order.shape)
+print(DW_Character_Order.columns)
+
+# Merge the tables and compare columns
+DW_Orders_Merge = pd.merge(DW_Publication_Order,DW_Character_Order,how='inner',left_on = 'Book_Id',right_on = 'DW_ID')
+print(DW_Orders_Merge.shape)
+print(DW_Orders_Merge.columns)
+print(DW_Orders_Merge.dtypes) # find out year / publication_date
+
+# Check title names
+for i in DW_Orders_Merge.itertuples():
+    if i[2] != i[5]:
+        print('Title ',i[0],' (',i[2],') not equal (',i[5],')')
+
+# Check publication year
+for i in DW_Orders_Merge.itertuples():
+    if str(i[3]) != str(i[10]):
+        print('Year ',i[0],' (',i[3],') not equal (',i[10],')')
+
+# Result:
+# Titles matching, but one publication year is incorrect:
+# Year  36  ( 2007 ) not equal ( 2009 )
+# Having checked online, 2009 is correct, so need to replace date on Merge and Publication Order
+#DW_Publication_Order('Book_Year')[36] = '2009'
+DW_Orders_Merge.at[36,'Book_Year'] = '2009'
+DW_Publication_Order.loc[36,'Book_Year'] = '2009'
+
+# Check publication year
+print('Recheck whether years match')
+
+for i in DW_Orders_Merge.itertuples():
+    if str(i[3]) != str(i[10]):
+        print('Year ',i[0],' (',i[3],') not equal (',i[10],')')
+
 ##################################################
 # Plot a timeline of the books being published
 ##################################################
-plt.plot(DW_Publication_Order['Book_Title'],DW_Publication_Order['Book_Year'], marker="*", linestyle="-", color="r")
+plt.plot(DW_Orders_Merge['Short_Title'],DW_Orders_Merge['Book_Year'], marker="*", linestyle="-", color="r")
 plt.title("Timeline of Publication")
 plt.xticks(rotation=45)
 plt.xlabel("Publication Title")
 plt.ylabel("Book Year")
 
 plt.show()
+
 

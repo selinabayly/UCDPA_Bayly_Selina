91def3075996b2e5cc03b2a9dbd29cbebd658594
selinabayly <78105212+selinabayly@users.noreply.github.com>
2021-03-21 20:02:11 +0000

    Imported CSV file with book ratings and indexed

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import urllib.request
 
+pd.options.display.max_columns = 10
 ##################################################
 # Import Datasets
 ##################################################
@@ -58,14 +59,19 @@ DW_Character_Order = pd.read_csv(r'DW_Public_Character_Order.csv')
 
 # Check number of columns in the two tables = 41
 # Showing shape, columns and data types
-print('DW_Publication_Order shape: ', DW_Publication_Order.shape)
-print('DW_Publication_Order dtypes: ', DW_Publication_Order.dtypes)
-print('DW_Character_Order shape: ', DW_Character_Order.shape)
-print('DW_Character_Order dtypes: ', DW_Character_Order.dtypes)
+print('DW_Publication_Order shape:')
+print(DW_Publication_Order.shape)
+print('DW_Publication_Order dtypes:')
+print(DW_Publication_Order.dtypes)
+print('DW_Character_Order shape:')
+print(DW_Character_Order.shape)
+print('DW_Character_Order dtypes:')
+print(DW_Character_Order.dtypes)
 
 # Convert Book Year to numeric to make it easier for comparison and making bins on graphs
 DW_Publication_Order['Book_Year'] = DW_Publication_Order.Book_Year.astype('int64')
-print('DW_Character_Order dtypes: ', DW_Publication_Order.dtypes)
+print('DW_Publication_Order dtypes:')
+print(DW_Publication_Order.dtypes)
 
 # Merge the tables and compare columns
 DW_Orders_Merge = pd.merge(DW_Publication_Order,
@@ -73,18 +79,23 @@ DW_Orders_Merge = pd.merge(DW_Publication_Order,
                            how='inner',
                            left_on='Book_Id',
                            right_on='DW_ID')
-print('DW_Orders_Merge shape:', DW_Orders_Merge.shape)
-print('DW_Orders_Merge dtypes:', DW_Orders_Merge.dtypes)
+print('DW_Orders_Merge shape:')
+print(DW_Orders_Merge.shape)
+print('DW_Orders_Merge dtypes:')
+print(DW_Orders_Merge.dtypes)
 
+# We now know we have 41 books to work from.  Any merges / joins with book databases
+# should return 41 joins.
 # Check title names
+print('DW_Orders_Merge - Title and Year check')
 for i in DW_Orders_Merge.itertuples():
     if i[2] != i[5]:
         print('Title ', i[0], ' (', i[2], ') not equal (', i[5], ')')
 
 # Check publication year
 for i in DW_Orders_Merge.itertuples():
-    if i[3] != i[10]:
-        print('Year ', i[0], ' (', i[3], ') not equal (', i[10], ')')
+    if i[3] != i[11]:
+        print('Year ', i[0], ' (', i[3], ') not equal (', i[11], ')')
 
 # Result:
 # Titles matching, but one publication year is incorrect:
@@ -99,8 +110,8 @@ DW_Publication_Order.loc[36, 'Book_Year'] = 2009
 print('Recheck whether years match')
 
 for i in DW_Orders_Merge.itertuples():
-    if i[3] != i[10]:
-        print('Year ', i[0], ' (', i[3], ') not equal (', i[10], ')')
+    if i[3] != i[11]:
+        print('Year ', i[0], ' (', i[3], ') not equal (', i[11], ')')
 
 ##################################################
 # Plot a timeline of all the books being published
@@ -123,14 +134,18 @@ plt.ylabel("Book Title")
 ################################################################
 
 # Find out how many Characters there are
-print('All the Characters')
+print('All the Characters and the number of books per character')
+print('Unsorted and then sorted')
 
 # Print out the Characters so that we can extract each to its own dataframe
 print(pd.value_counts(DW_Orders_Merge.Character))
 
-# Save Characters, Colour and Label to list]
+# Save Characters, Colour and Label to list and sort
 DW_Character_List = DW_Orders_Merge[['Character', 'Colour', 'Label']].value_counts().index[:].tolist()
 print(DW_Character_List)
+DW_Character_List.sort()
+print(DW_Character_List)
+
 
 # Create function to create dataframes for Character
 def create_dw_orders_merge(l_character):
@@ -208,9 +223,36 @@ plt.xlabel("Publication Year", fontsize=16)
 plt.ylabel("Book Title", fontsize=16)
 plt.legend()
 
-plt.show()
+###################################################################
+# Review other files to see which ones will give the best results.
+###################################################################
+book_data_kaggle = pd.read_csv(r'book_data_kaggle_subset.csv')
+print('book_data_kaggle - dtypes and shape')
+print(book_data_kaggle.dtypes)
+print(book_data_kaggle.shape)
 
-################################################################
-# Plot a timeline of all the books being published by character
-# Adding full series to try and she character crossover
-################################################################
+# Print subset of book_data_kaggle and see what type of structure it is
+
+print(type(book_data_kaggle.iloc[:, [0, 9]]))
+print(book_data_kaggle.iloc[:5, [0, 9]])
+
+print(type(DW_Orders_Merge.iloc[:, [1, 5]]))
+print(DW_Orders_Merge.iloc[:5, [1, 5]])
+
+book_data_kaggle_join = pd.merge(DW_Orders_Merge, book_data_kaggle,
+                                 how='inner', left_on=('Book_Title', 'Author'),
+                                 right_on=('book_title', 'book_authors'))
+print('book_data_kaggle_join - dtypes, shape, head, index')
+print(book_data_kaggle_join.dtypes)
+print(book_data_kaggle_join.shape)
+print(book_data_kaggle_join.head())
+print(book_data_kaggle_join.index)
+print('')
+print(book_data_kaggle_join.iloc[:5, [1, 4, 5, 15, 24]])
+book_data_kaggle_join.set_index('Author', inplace=True)
+print(book_data_kaggle_join.index)
+
+print(book_data_kaggle_join.loc[['Terry Pratchett']])
+
+# plt.show()
+# end
